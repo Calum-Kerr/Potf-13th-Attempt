50a6c58e089c5c959657b199cfc73639be717055
Calum-Kerr <calumxkerr@gmail.com>
2024-11-27 20:50:03 +0000

    basic sfml window setup using official tutorial link

--- Potf-13th-Attempt/Main.cpp
+++ Potf-13th-Attempt/Main.cpp
@@ -1,24 +1,14 @@
-#include <SFML/Graphics.hpp>
-
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
+#include<SFML/Graphics.hpp>
+// reference: official sfml tutorial for window creation (https://www.sfml-dev.org/tutorials/2.5/start-linux.php)
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Path of the Foresaken");
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)window.close();
         }
-
         window.clear();
-        window.draw(shape);
         window.display();
     }
-
     return 0;
 }
